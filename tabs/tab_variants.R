d0c21cd7a18bd698e7070a0ba7f9eea8ffba21d5
Kyla Belisario <71769015+kbelisar@users.noreply.github.com>
2021-05-05 20:38:27 -0400

    Add files via upload

--- tabs/tab_variants.R
+++ tabs/tab_variants.R
@@ -9,8 +9,9 @@ tab_variants <- tabItem(tabName = "tab_variants",
                      ),HTML("<center><h1 style='font-size:27px'>Trend of Reported VOC+ Cases</h1></center>
                             <p>Regular COVID-19 tests are unable to identify the presence of VOCs. Specimens which have tested positive for COVID-19 often need to be sent for additional testing to determine the strain of the virus.
                             As such, it can be difficult to make direct comparisons of VOCs vs non-VOCs, as further testing results in delays between the initial reporting of COVID-19 positivity and VOC positivity.</p>
-                            <p>VOC reporting has been inconsistent across Canada. It is also inconsistent across time, with no VOC data updates on weekends resulting in zero reported cases on these days.
-                            As such, we suggest using the 7-day rolling averages of daily report cases and VOC cases, as this allows for the overall trend of VOCs to be visualized.</p>"),
+                            <p>VOC reporting has been inconsistent across Canada, and also inconsistent across time. There are no VOC data updates on weekends, resulting in zero reported cases on these days. Additionally, due to the
+                            delay between reported COVID-19 positivity and reported VOCs, the number of VOCs can exceed the number of cases reported. We suggest using the 7-day rolling averages of daily report cases and VOC cases, 
+                            as this allows for the overall trend of VOCs to be visualized.</p>"),
                               fluidRow(box(
                                 title = textOutput("title_daily_voc"),
                                 width = 12,
