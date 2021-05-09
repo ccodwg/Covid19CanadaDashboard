1189e6c7c2663956d845c78a7031438e09959e14
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-05-09 16:03:19 -0400

    Make ui a function to prevent UI caching.
    This may prevent the dashboard from showing old data even after restarting the server. #18
    
    https://stackoverflow.com/a/57325250

--- ui.R
+++ ui.R
@@ -10,72 +10,73 @@ js_get_width <-
 '
 
 # ui
-ui <- dashboardPagePlus(
-  title = "COVID-19 in Canada",
-  dashboardHeader(title = "COVID-19 in Canada"),
-  dashboardSidebar(
-    useShinyjs(),
-    sidebarMenu(
-      id = "tab",
-      uiOutput("sidebar_controls"),
-      menuItem("Overview",
-               tabName = "tab_overview",
-               icon = icon("table")),
-      # menuItem(
-      #   "Trends",
-      #   tabName = "tab_trends",
-      #   icon = icon("chart-line")
-      # ),
-      menuItem(
-        "Comparisons",
-        tabName = "tab_comparisons",
-        icon = icon("chart-bar")
-      ),
-      menuItem(
-        "Maps",
-        tabName = "tab_maps",
-        icon = icon("map-marked-alt")
-      ),
-      menuItem("Vaccines",
-               tabName = "tab_vaccines",
-               icon = icon("syringe")
-      ),
-      menuItem(
-        "Cases",
-        tabName = "tab_cases",
-        icon = icon("calendar-plus")
-      ),
-      menuItem(
-        "Mortality",
-        tabName = "tab_mortality",
-        icon = icon("heartbeat")
-      ),
-      menuItem(
-        "Recovered",
-        tabName = "tab_recovered",
-        icon = icon("file-medical")
-      ),
-      menuItem("Testing",
-               tabName = "tab_testing",
-               icon = icon("vial")),
-      menuItem(
-        "About the data",
-        tabName = "tab_about",
-        icon = icon("question-circle")
-      ),
-      useShinyalert(),
-      tags$footer(
-        HTML(
-          paste0(
-            "<b>Please visit our <a href='https://opencovid.ca/work/data-faq/' target='_blank'> frequently<br/>asked questions</a> page</br>about our dataset for answers</br>to questions like this one:<br/></b>",
-            sapply(strwrap(faq, 30, simplify = FALSE), paste, collapse = "<br/>"),
-            "<b><br/><br/><a href='https://github.com/ishaberry/Covid19Canada' target='_blank'> Download our dataset</a><br/><br/>Last updated:<br/>",
-            update_time,
-            "</b><br/><br/>All data used to produce this<br/>dashboard are exclusively<br/>collected from publicly available<br/>sources including government<br/>reports and news media.<br/>"
-          )
+ui <- function(req) {
+  dashboardPagePlus(
+    title = "COVID-19 in Canada",
+    dashboardHeader(title = "COVID-19 in Canada"),
+    dashboardSidebar(
+      useShinyjs(),
+      sidebarMenu(
+        id = "tab",
+        uiOutput("sidebar_controls"),
+        menuItem("Overview",
+                 tabName = "tab_overview",
+                 icon = icon("table")),
+        # menuItem(
+        #   "Trends",
+        #   tabName = "tab_trends",
+        #   icon = icon("chart-line")
+        # ),
+        menuItem(
+          "Comparisons",
+          tabName = "tab_comparisons",
+          icon = icon("chart-bar")
         ),
-        align = "left",
-        style = "
+        menuItem(
+          "Maps",
+          tabName = "tab_maps",
+          icon = icon("map-marked-alt")
+        ),
+        menuItem("Vaccines",
+                 tabName = "tab_vaccines",
+                 icon = icon("syringe")
+        ),
+        menuItem(
+          "Cases",
+          tabName = "tab_cases",
+          icon = icon("calendar-plus")
+        ),
+        menuItem(
+          "Mortality",
+          tabName = "tab_mortality",
+          icon = icon("heartbeat")
+        ),
+        menuItem(
+          "Recovered",
+          tabName = "tab_recovered",
+          icon = icon("file-medical")
+        ),
+        menuItem("Testing",
+                 tabName = "tab_testing",
+                 icon = icon("vial")),
+        menuItem(
+          "About the data",
+          tabName = "tab_about",
+          icon = icon("question-circle")
+        ),
+        useShinyalert(),
+        tags$footer(
+          HTML(
+            paste0(
+              "<b>Please visit our <a href='https://opencovid.ca/work/data-faq/' target='_blank'> frequently<br/>asked questions</a> page</br>about our dataset for answers</br>to questions like this one:<br/></b>",
+              sapply(strwrap(faq, 30, simplify = FALSE), paste, collapse = "<br/>"),
+              "<b><br/><br/><a href='https://github.com/ishaberry/Covid19Canada' target='_blank'> Download our dataset</a><br/><br/>Last updated:<br/>",
+              update_time,
+              "</b><br/><br/>All data used to produce this<br/>dashboard are exclusively<br/>collected from publicly available<br/>sources including government<br/>reports and news media.<br/>"
+            )
+          ),
+          align = "left",
+          style = "
               position:absolute;
               width:100%;
               height:0px;
@@ -84,24 +85,24 @@ ui <- dashboardPagePlus(
               background-color: transparent;
               word-wrap: break-word;
               z-index: 1000;"
+        )
       )
-    )
-  ),
-  dashboardBody(
-    meta() %>%
-      meta_social(
-        title = "COVID-19 in Canada",
-        description = "An interactive visualization of the ongoing epidemic of COVID-19 in Canada",
-        url = "https://art-bd.shinyapps.io/covid19canada/",
-        image = "https://source.unsplash.com/VhjsGKMefkk/640x426",
-        image_alt = "COVID-19"
-      ),
-    ### google-analytics.html is not needed to run app locally
-    analytics,
-    tags$script(js_get_width),
-    tags$head(tags$style(
-      HTML(
-        '.myClass {
+    ),
+    dashboardBody(
+      meta() %>%
+        meta_social(
+          title = "COVID-19 in Canada",
+          description = "An interactive visualization of the ongoing epidemic of COVID-19 in Canada",
+          url = "https://art-bd.shinyapps.io/covid19canada/",
+          image = "https://source.unsplash.com/VhjsGKMefkk/640x426",
+          image_alt = "COVID-19"
+        ),
+      ### google-analytics.html is not needed to run app locally
+      analytics,
+      tags$script(js_get_width),
+      tags$head(tags$style(
+        HTML(
+          '.myClass {
         font-size: 20px;
         line-height: 50px;
         text-align: left;
@@ -111,34 +112,35 @@ ui <- dashboardPagePlus(
         color: white;
       }
     '
-      )
-    )),
-    tags$script(
-      HTML(
-        '
+        )
+      )),
+      tags$script(
+        HTML(
+          '
       $(document).ready(function() {
         $("header").find("nav").append(\'<span class="myClass"> Menu </span>\');
       })
      '
+        )
+      ),
+      tabItems(
+        tab_overview,
+        # tab_trends,
+        tab_comparisons,
+        tab_maps,
+        tab_vaccines,
+        tab_cases,
+        tab_mortality,
+        tab_recovered,
+        tab_testing,
+        tab_about
       )
     ),
-    tabItems(
-      tab_overview,
-      # tab_trends,
-      tab_comparisons,
-      tab_maps,
-      tab_vaccines,
-      tab_cases,
-      tab_mortality,
-      tab_recovered,
-      tab_testing,
-      tab_about
-    )
-  ),
-  dashboardFooter(
-    left_text = HTML(
-      "Created by <a href='https://twitter.com/JPSoucy' target='_blank'> Jean-Paul R. Soucy</a> and <a href='https://twitter.com/ishaberry2' target='_blank'> Isha Berry</a>, Dalla Lana School of Public Health, University of Toronto. Developed by Jean-Paul R. Soucy, Isha Berry, <a href='https://twitter.com/benkcwong' target='_blank'> Ben Wong</a> and <a href='https://github.com/kbelisar' target='_blank'> Kyla Belisario</a>, with contributions from <a href='https://www.linkedin.com/in/wenyu-huang-11b211124/' target='_blank'> Wenyu Huang</a>, <a href='https://www.linkedin.com/in/angelinazhu' target='_blank'> Angelina Zhu</a>, <a href='https://github.com/mattwarkentin' target='_blank'> Matthew T. Warkentin</a> and <a href='https://mountainmath.ca/' target='_blank'> Jens von Bergmann</a>. Data curated by the <a href='https://opencovid.ca' target='_blank'> COVID-19 Canada Open Data Working Group</a> (see \"About the data\" for sources)."
-    )
-  ),
-  sidebar_fullCollapse = TRUE
-)
+    dashboardFooter(
+      left_text = HTML(
+        "Created by <a href='https://twitter.com/JPSoucy' target='_blank'> Jean-Paul R. Soucy</a> and <a href='https://twitter.com/ishaberry2' target='_blank'> Isha Berry</a>, Dalla Lana School of Public Health, University of Toronto. Developed by Jean-Paul R. Soucy, Isha Berry, <a href='https://twitter.com/benkcwong' target='_blank'> Ben Wong</a> and <a href='https://github.com/kbelisar' target='_blank'> Kyla Belisario</a>, with contributions from <a href='https://www.linkedin.com/in/wenyu-huang-11b211124/' target='_blank'> Wenyu Huang</a>, <a href='https://www.linkedin.com/in/angelinazhu' target='_blank'> Angelina Zhu</a>, <a href='https://github.com/mattwarkentin' target='_blank'> Matthew T. Warkentin</a> and <a href='https://mountainmath.ca/' target='_blank'> Jens von Bergmann</a>. Data curated by the <a href='https://opencovid.ca' target='_blank'> COVID-19 Canada Open Data Working Group</a> (see \"About the data\" for sources)."
+      )
+    ),
+    sidebar_fullCollapse = TRUE
+  )
+}
